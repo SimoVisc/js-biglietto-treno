cb6daed5a7ce6e5f7ca23d0808fe79e033b2b88c
SimoVisc <simonaviscardivs@gmail.com>
2023-04-04 21:55:18 +0200

    Aggiunge validazione

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 "use strict";
-// chiedi il numero di km da percorrere e l'età del passeggero
-const km = Number (prompt ("inserisci il numero di km da percorrere"));
-const age = Number (prompt ("inserisci la tua età"));
-console.log(km,age);
-// calcola il prezzo totale del viaggio, seguendo queste regole
-// il prezzo totale del biglietto è definito in base al KM(0.21€ al km)
-let price = km * 0.21;
-let message = "prezzo del biglietto";
-// va applicato uno sconto del 20% per i minorenni 
-// va applicato uno sconto del 40% per gli over65
-if (age < 18){
-    price = price - price * 0.2;
-    message = "prezzo tariffa minorenni";
-}else if (age >= 65){
-    price = price - price * 0.4;
-    message = "prezzo tariffa over 65";
+// chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+const km = Number(prompt("Inserisci i km da percorrere"));
+const age = Number(prompt("Inserisci la tua età"));
+if (isNaN(km) || isNaN(age) || km <= 0 || age <= 0) {
+  alert("Inserisci dei numeri validi!");
+  window.location.reload();
 }
-console.log('${price.toFixed(2)}€')
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+let price = km * 0.21;
+let message = "biglietto tariffa intera";
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+if (age < 18) {
+  price -= price * 0.2;
+  message = "biglietto tariffa minorenni";
+} else if (age >= 65) {
+  price -= price * 0.4;
+  message = "biglietto tariffa over 65";
+}
+
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+console.log(`${price.toFixed(2)}€`);
+console.log(message);
